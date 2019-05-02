01b6cee3f1497c160e42b6b80f1dbe641e87b9ed
Administrator <Administrator@USER-20170207LK>
2019-05-02 09:37:11 +0800

    快速开始 hello world

---
+++ rabbitmq-api/src/main/java/com/qqxhb/rabbitmq/quickstart/Consumer.java
@@ -0,0 +1,43 @@
+package com.qqxhb.rabbitmq.quickstart;
+
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+import com.rabbitmq.client.QueueingConsumer;
+import com.rabbitmq.client.QueueingConsumer.Delivery;
+
+public class Consumer {
+
+	public static void main(String[] args) throws Exception {
+		
+		/*
+		 * 1、创建一个ConnectionFactory，设置主机及端口
+		 * 2、通过工厂创建连接
+		 * 3、通过连接 创建通道Channel
+		 * 4、通过通道声明队列
+		 * 5、创建消费者
+		 * 6、给通道设置消费者及队列
+		 * 7、获取消息
+		 */
+		ConnectionFactory factory = new ConnectionFactory();
+		factory.setHost("127.0.0.1");
+		factory.setPort(5672);
+		factory.setVirtualHost("/");
+		
+		Connection connection = factory.newConnection();
+		
+		Channel channel = connection.createChannel();
+		String queueName = "quickstart";
+		channel.queueDeclare(queueName, true, false, false, null);
+		
+		QueueingConsumer consumer = new QueueingConsumer(channel);
+		
+		channel.basicConsume(queueName, true,consumer);
+		
+		while (true) {
+			Delivery delivery = consumer.nextDelivery();
+			String msg = new String(delivery.getBody());
+			System.out.println(msg);
+		}
+	}
+}
