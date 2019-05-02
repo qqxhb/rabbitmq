152f727bb259ec8e0abc8f2cb15573f51fb9cb79
Administrator <Administrator@USER-20170207LK>
2019-05-02 14:45:58 +0800

    springboot 整合 RabbitMQ

---
+++ springboot/src/main/java/com/qqxhb/springboot/MainConfig.java
@@ -0,0 +1,10 @@
+package com.qqxhb.springboot;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan({"com.qqxhb.springboot.*"})
+public class MainConfig {
+
+}
